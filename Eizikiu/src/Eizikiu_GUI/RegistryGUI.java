6f24b98e5346a96e6c6e6c0c389694c52436df3f
Benita <Benita@LAPTOP-FA6QUBJI.fritz.box>
2018-01-06 15:42:24 +0100

    Der erste Versuch eines Fensters

---
+++ Eizikiu/src/Eizikiu_GUI/RegistryGUI.java
@@ -0,0 +1,48 @@
+package Eizikiu_GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RegistryGUI extends JFrame implements ActionListener {
+	public RegistryGUI(){
+		super();
+		
+		setTitle("Eizikiu Registry");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel text1 = new JLabel("Geben sie den gewünschten Usernamen an:");
+		JLabel text2 = new JLabel("Geben sie ihr Password an:");
+		JLabel text3 = new JLabel("Wiederholen sie ihr Passwort");
+				
+		JTextField textfieldName = new JTextField();
+		JTextField textfieldPassword = new JTextField();
+		JTextField textfieldPasswordRepeat = new JTextField();
+		
+		JButton register = new JButton();
+		
+		this.setLayout(new FlowLayout());
+		
+		this.add(text1);
+		this.add(textfieldName);
+		this.add(text2);
+		this.add(textfieldPassword);
+		this.add(text3);
+		this.add(textfieldPasswordRepeat);
+		this.add(register);
+		
+		this.pack();
+		
+	}
+	
+	public void actionPerformed(){
+		
+	}
+	public static void main(String[] args){
+		RegistryGUI Registrierung = new RegistryGUI();
+		Registrierung.setSize(300,150);
+		Registrierung.setLocation(800,300);
+		Registrierung.setVisible(true);
+	}
+}
+
+}
